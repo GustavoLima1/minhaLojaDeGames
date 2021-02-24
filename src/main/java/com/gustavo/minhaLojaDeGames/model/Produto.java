86ab5862932fd4857b7c6c13fc1ed42244cace5b
Gustavo Lima <gustavo1.l@hotmail.com>
2021-02-24 11:22:24 -0300

    Inicializando a classe produto

---
+++ src/main/java/com/gustavo/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,75 @@
+package com.gustavo.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table( name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 25)
+	private String nome;
+	
+	@NotNull
+	private int qtdEstoque;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	@Size(min = 2, max = 25)
+	private String plataforma;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(int qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	
+	
+}
