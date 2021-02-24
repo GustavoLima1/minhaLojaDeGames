0bedbb055490302c52893be4a051d631e46428aa
Gustavo Lima <gustavo1.l@hotmail.com>
2021-02-24 18:46:00 -0300

    repository e controller implementados

--- src/main/java/com/gustavo/minhaLojaDeGames/model/Usuario.java
+++ src/main/java/com/gustavo/minhaLojaDeGames/model/Usuario.java
@@ -1,11 +1,15 @@
 package com.gustavo.minhaLojaDeGames.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.sun.istack.NotNull;
 
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario {
 	
 	@Id
@@ -13,7 +17,7 @@ public class Usuario {
 	private long id;
 	
 	@NotNull
-	private String nome_completo;
+	private String nome;
 	@NotNull
 	private String email;
 	@NotNull
@@ -25,10 +29,10 @@ public class Usuario {
 		this.id = id;
 	}
 	public String getNome_completo() {
-		return nome_completo;
+		return nome;
 	}
-	public void setNome_completo(String nome_completo) {
-		this.nome_completo = nome_completo;
+	public void setNome_completo(String nome) {
+		this.nome = nome;
 	}
 	public String getEmail() {
 		return email;
--- src/main/java/com/gustavo/minhaLojaDeGames/repositories/UsuarioRepository.java
+++ src/main/java/com/gustavo/minhaLojaDeGames/repositories/UsuarioRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gustavo.minhaLojaDeGames.model.Produto;
+import com.gustavo.minhaLojaDeGames.model.Usuario;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	List <Produto> findAllByNomeContainingIgnoreCase(String nome);
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public List <Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
+
