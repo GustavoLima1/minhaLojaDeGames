6498750e7e60fc7b007f72162950ba62df15793e
Gustavo Lima <gustavo1.l@hotmail.com>
2021-02-24 10:40:59 -0300

    Iniciando o projeto com model a categoria

---
+++ src/main/java/com/gustavo/minhaLojaDeGames/model/Categoria.java
@@ -0,0 +1,56 @@
+package com.gustavo.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table (name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size( min = 2, max = 25)
+	private String console;
+	
+	@NotNull
+	@Size( min = 2, max = 25)
+	private String categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+
+}
